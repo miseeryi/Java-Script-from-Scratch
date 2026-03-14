9e7ae6aae059066e0fe4ef92f942286fda32ae1d
Alfaraby <alfrby09@gmail.com>
2026-03-14 16:06:47 +0700

    day-1 The Beginning

---
+++ Java Script - basic/01 - Awal bgt/indeks.js
@@ -0,0 +1,10 @@
+console.log(`Hallo`)
+console.log(`HALLO!`)
+// output keluarnya di console
+
+window.alert(`Notif Pop Up`);
+window.alert(`TEST`);
+// ini untuk alert/pop up 
+
+document.getElementById("myh1").textContent = `String 1`;
+document.getElementById("myp").textContent = `String 2`;
